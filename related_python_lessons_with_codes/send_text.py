b7cc5ec9d448236ee810e72c7a5a40779de15b7f
MomokoXu <momoko0808@hotmail.com>
2017-03-16 16:19:42 -0700

    [update]program as a messages sender using twilio

--- related_python_lessons_with_codes/send_text.py
+++ related_python_lessons_with_codes/send_text.py
@@ -1,8 +1,8 @@
 from twilio.rest import TwilioRestClient
 # equavlent to from twilio import rest, then client = rest.TwilioRestClient(..., ...)
 
-account_sid = "AC13a66020fba9943b41f63906d2b78d37"
-auth_token = "8e999f0e5716a42fbd34615a3aab7a80"
+account_sid = "AC13a66020fba9943b4XXXXXXXYOURSID"
+auth_token = "8e999f0e5716a42fbd3461XXXXXYOURAUTHTOKEN"
 client = TwilioRestClient(account_sid, auth_token)
 
 message = client.sms.messages.create(body = "Lalalala!Fighting!", to = "+19295227155", from_ = "+14156500808")
